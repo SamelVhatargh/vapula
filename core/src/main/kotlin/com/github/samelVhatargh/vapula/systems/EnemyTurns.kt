7f2937ee25f9c51a04cbea7a4f40046d195a0737
Alexander Byankin <samel@bk.ru>
2020-06-23 18:38:29 +1100

    Реализовано передвижение монстров в рандомном направлении

--- core/src/main/kotlin/com/github/samelVhatargh/vapula/systems/EnemyTurns.kt
+++ core/src/main/kotlin/com/github/samelVhatargh/vapula/systems/EnemyTurns.kt
@@ -5,14 +5,27 @@ import com.badlogic.ashley.systems.IteratingSystem
 import com.github.samelVhatargh.vapula.GameState
 import com.github.samelVhatargh.vapula.components.Ai
 import com.github.samelVhatargh.vapula.components.Name
+import com.github.samelVhatargh.vapula.components.Position
+import com.github.samelVhatargh.vapula.systems.commands.Move
+import com.github.samelVhatargh.vapula.systems.commands.MoveDirection
 import ktx.ashley.allOf
-import ktx.ashley.get
-import ktx.log.debug
+import ktx.ashley.getSystem
 import ktx.log.logger
 
 class EnemyTurns(private val gameState: GameState) : IteratingSystem(
-    allOf(Ai::class, Name::class).get()
+    allOf(Ai::class, Name::class, Position::class).get()
 ) {
+
+    private val directions = listOf(
+        MoveDirection.NORTH,
+        MoveDirection.SOUTH,
+        MoveDirection.EAST,
+        MoveDirection.WEST,
+        MoveDirection.NORTH_EAST,
+        MoveDirection.NORTH_WEST,
+        MoveDirection.SOUTH_EAST,
+        MoveDirection.SOUTH_WEST
+    )
 
     companion object {
         val log = logger<EnemyTurns>()
@@ -25,6 +38,8 @@ class EnemyTurns(private val gameState: GameState) : IteratingSystem(
     }
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
-        log.debug { "${entity[Name.mapper]!!.name} acts" }
+        val direction = directions.random()
+
+        engine.getSystem<Move>().execute(entity, direction)
     }
 }
