606c4ae90ec5bc01d309c816347b1ce991fb941d
Alexander Byankin <samel@bk.ru>
2020-10-24 15:53:54 +1100

    Хп отображается справа вверху

--- core/src/main/kotlin/com/github/samelVhatargh/vapula/ui/Hud.kt
+++ core/src/main/kotlin/com/github/samelVhatargh/vapula/ui/Hud.kt
@@ -1,7 +1,6 @@
 package com.github.samelVhatargh.vapula.ui
 
 import com.badlogic.gdx.scenes.scene2d.ui.Label
-import com.badlogic.gdx.utils.Align
 import com.github.samelVhatargh.vapula.components.Stats
 import ktx.scene2d.label
 import ktx.scene2d.scene2d
@@ -12,14 +11,16 @@ class Hud {
     private var hp: Label
 
     val panel = scene2d.table {
-        defaults().fillX().expandX()
-        hp = label("HP: ") {
-            setAlignment(Align.center)
+        table {
+            right().top().cell(growY = true)
+
+            hp = label("HP: ") { cell ->
+                cell.width(4 * 64f - 2 * 8f).pad(8f)
+            }
         }
 
-        top()
+        right()
         setFillParent(true)
-        pack()
     }
 
     fun updatePlayerStats(stats: Stats) {
